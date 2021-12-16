4960d3bbb747e888fc01ddc4f92792a514f67d05
athaeck <philipp.haecker@web.de>
2021-12-16 23:12:11 +0100

    nvm

--- VirtuelPrototype/Assets/Scripts/MovementScene/Player.cs
+++ VirtuelPrototype/Assets/Scripts/MovementScene/Player.cs
@@ -36,55 +36,50 @@ public class Player : MonoBehaviour
 
         _interactions.Add(interaction);
 
-   
+
 
     }
     public void ReconstructRecord(float timestamp)
     {
 
-        if(timestamp < _lastTimestamp)
+
+        InteractionSaveData interaction = _interactions.Find(i => (_lastTimestamp < i.TimeStamp && i.TimeStamp <= timestamp));
+
+        SetLastTimestamp(timestamp);
+
+
+        if(interaction != null)
         {
-            InteractionSaveData interaction = _interactions.Find(i => (_lastTimestamp < i.TimeStamp && i.TimeStamp <= timestamp));
-
-            //SetLastTimestamp(timestamp);
-
-
-            if(interaction != null)
+            InteractionType type = (InteractionType)Enum.Parse(typeof(InteractionType),interaction.Type);
+            switch(type)
             {
-                InteractionType type = (InteractionType)Enum.Parse(typeof(InteractionType),interaction.Type);
-                switch(type)
-                {
-                    case InteractionType.WALK:
+                case InteractionType.WALK:
 
-                        transform.position = new Vector3(interaction.Source.Position[0],interaction.Source.Position[1],interaction.Source.Position[2]);
-                        transform.rotation = Quaternion.Euler(new Vector3(interaction.Source.Rotation[0],interaction.Source.Rotation[1],interaction.Source.Rotation[2]));
-                        transform.localScale = new Vector3(interaction.Source.Scale[0],interaction.Source.Scale[1],interaction.Source.Scale[2]);
+                    transform.position = new Vector3(interaction.Source.Position[0],interaction.Source.Position[1],interaction.Source.Position[2]);
+                    transform.rotation = Quaternion.Euler(new Vector3(interaction.Source.Rotation[0],interaction.Source.Rotation[1],interaction.Source.Rotation[2]));
+                    transform.localScale = new Vector3(interaction.Source.Scale[0],interaction.Source.Scale[1],interaction.Source.Scale[2]);
 
-                        break;
-                    default:
-                        break;
-                }
+                    break;
+                default:
+                    break;
             }
         }
-        else
-        {
-            DestroyShadow?.Invoke(this.gameObject);
-        }
-     
 
-       
+
+
+
 
     }
     public void SetLastTimestamp(float timestamp)
     {
-        
+
         _lastTimestamp = timestamp;
     }
 
     public void InsertInteractions(List<InteractionSaveData> interactions)
     {
         _interactions = interactions;
-        _lastTimestamp = _interactions[_interactions.Count - 1].TimeStamp;
+
     }
     public CharacterData GetCharacterData()
     {
